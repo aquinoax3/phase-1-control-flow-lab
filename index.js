af0b59acb67081672babeb8c095854fcaf2e02a6
Axel Aquino <aquino.axel@gmail.com>
2022-01-30 21:25:12 -0500

    commit tree 4a78b3e7ab6f359c755cae2c193d48a37492b2bd

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetRide){
+
+  if (feetRide <= 400) {
+    return 'This one is on me!';
+
+  } else if (feetRide > 2500) {
+    return 'No can do.';
+    
+  } else if (feetRide > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+
+  }
 }
